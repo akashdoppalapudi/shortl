518db4ed9a4e6d0580e140397abc1b47cb4fe27c
Akash Doppalapudi <akashdoppalapudi2001@gmail.com>
2022-07-22 20:10:47 +0530

    index will now redirect to create page

--- middleware.ts
+++ middleware.ts
@@ -8,6 +8,10 @@ const middleware = async (req: NextRequest, ev: NextFetchEvent) => {
 		return;
 	}
 
+	if (req.nextUrl.pathname === '/') {
+		return NextResponse.redirect(`${req.nextUrl.origin}/create`); // redirect to /create is temporary... may remove in future
+	}
+
 	const slug = req.nextUrl.pathname.split('/').pop();
 	if (
 		!slug ||
